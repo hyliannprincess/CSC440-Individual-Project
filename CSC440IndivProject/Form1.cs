d27417e99db3e2e1f04b28905cf9a6bd77ff09c1
hyliannprincess <kyleea698@gmail.com>
2024-11-23 22:22:21 -0500

    Added UI

--- CSC440IndivProject/Form1.cs
+++ CSC440IndivProject/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+//using Excel = Microsoft.Office.Interop.Excel;
 
 
 namespace CSC440IndivProject
@@ -17,46 +18,76 @@ namespace CSC440IndivProject
         public Form1()
         {
             InitializeComponent();
+            panel1.Hide();
+            panel2.Hide();
+            panel3.Hide();
+            panel4.Hide();
+            panel5.Hide();
+            panel6.Hide();
+            panel7.Hide();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void listBox1_DoubleCLick(object sender, EventArgs e)
         {
 
         }
 
-        private void label6_Click(object sender, EventArgs e)
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void importGrades_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click_1(object sender, EventArgs e)
+
+        private void addGrade_Click(object sender, EventArgs e)
+        {
+            panel1.Show();
+
+        }
+        private void submitNewGrade_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+
+        private void editGrade_Click(object sender, EventArgs e)
+        {
+            panel4.Show();
+        }
+
+        private void deleteGrade_Click(object sender, EventArgs e)
+        {
+            panel6.Show();
+        }
+        private void deleteGradeSubmit_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void printTranscript_Click(object sender, EventArgs e)
         {
-
+            panel7.Show();
         }
+
+
+
+
     }
 }
